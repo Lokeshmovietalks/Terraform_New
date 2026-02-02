5ffa748ffa9a90595b9fe5a471e788ad920e1c08
EC2 Default User <ec2-user@ip-172-31-0-26.ap-southeast-2.compute.internal>
2026-02-02 09:46:44 +0000

    this is a new change

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "Master_1" {
   key_name      = "January"
-  instance_type = "m7i-flex.large"
+  instance_type = "t3.small"
   ami           = "ami-048ab8ac7e8c6533d"
   root_block_device {
     volume_size           = 20
